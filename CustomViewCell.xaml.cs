ac33dc22829d815f59351849a31c0f75c74bf2d1
ssombat <ssombat@users.noreply.github.com>
2019-05-07 09:06:41 -0400

    Add files via upload

---
+++ CustomViewCell.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App2
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CustomViewCell : ViewCell
+    {
+        private string _name;
+        public string Name
+        {
+            get => _name;
+        }
+        public CustomViewCell(string name)
+        {
+            _name = name;
+            InitializeComponent();
+            setNameInGui();
+        }
+
+        private void setNameInGui()
+        {
+            label.Text = Name;
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App2
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(true)]
+    public partial class MainPage : ContentPage
+    {
+        ObservableCollection<CustomViewCell> observableCollection = new ObservableCollection<CustomViewCell>(); 
+        public MainPage()
+        {
+            InitializeComponent();
+            listView.ItemsSource = observableCollection;
+
+            observableCollection.Add(new CustomViewCell("test1"));
+            observableCollection.Add(new CustomViewCell("test2"));
+            observableCollection.Add(new CustomViewCell("test3"));
+            observableCollection.Add(new CustomViewCell("test4"));
+            observableCollection.Add(new CustomViewCell("test5"));
+            observableCollection.Add(new CustomViewCell("test6"));
+        }
+    }
+}
